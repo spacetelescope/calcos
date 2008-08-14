d8c613537617ded014c7f98640333b810e7b3119
sienkiew <sienkiew@stsci.edu>
2008-08-14 21:42:15 +0000

    setup.py mods - more changes
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/trunk/calcos@6930 fe389314-cf27-0410-b35b-8c050e845b92

---
+++ defsetup.py
@@ -0,0 +1,27 @@
+import distutils.core
+import distutils
+
+try:
+    import numpy
+except ImportError:
+    print "NUMPY was not found. It may not be installed or it may not be on your PYTHONPATH"
+
+pkg = "calcos"
+
+setupargs = {
+    'version' :			"1.0",
+    'description' :		"C extension module for calcos",
+    'author' :			"Phil Hodge",
+    'author_email' :	"help@stsci.edu",
+    'platforms' :		["Linux", "Solaris", "Mac OS X", "Windows"],
+    'scripts' :			['lib/calcos.py'],
+    'ext_modules' :		[ 
+                            distutils.core.Extension ("calcos.ccos", ["src/ccos.c"],
+                               define_macros = [('NUMPY', '1')],
+                               include_dirs = [ distutils.sysconfig.get_python_inc(), 
+                                                numpy.get_include()
+                                              ]
+                            )
+                        ],
+}
+
