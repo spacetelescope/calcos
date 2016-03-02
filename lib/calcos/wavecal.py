spacetelescope/calcos

commit d2235a9b54c506f9b7b0a9c4fd3953b8ddda4b6b

Fix #1250
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/calcos/trunk@49353 fe389314-cf27-0410-b35b-8c050e845b92

lib/calcos/wavecal.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division         # confidence high
 import math
 import os
 import numpy as np
-from stsci.convolve import boxcar
+from scipy.signal import boxcar
 import astropy.io.fits as fits
 from .calcosparam import *
 from . import ccos

lib/calcos/xd_search.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division         # confidence unknown
 import numpy as np
-from stsci.convolve import boxcar
+from scipy.signal import boxcar
 from . import cosutil
 from .calcosparam import *       # parameter definitions
 from . import ccos

setup.cfg
@@ -1,12 +1,12 @@
 [metadata]
 name = calcos
-version = 3.1.1
-vdate = 2015-11-02
+version = 3.1.2
+vdate = 2016-02-24
 author = Phil Hodge and Robert Jedrzejewski
 author-email = help@stsci.edu
 summary = Calibration software for COS (Cosmic Origins Spectrograph)
 home-page = http://www.stsci.edu/resources/software_hardware/stsci_python
-classifier = 
+classifier =
 	Intended Audience :: Science/Research
 	License :: OSI Approved :: BSD License
 	Operating System :: OS Independent
@@ -14,20 +14,21 @@ classifier =
 	Topic :: Scientific/Engineering :: Astronomy
 	Topic :: Software Development :: Libraries :: Python Modules
 requires-python = >=2.6
-requires-dist = 
+requires-dist =
 	numpy (>=1.5.1)
 	astropy (>=0.3.1)
+        scipy (>=0.14)
 
 [files]
 packages_root = lib
 packages = calcos
-package_data = 
+package_data =
 	calcos = pars/* *.help
 scripts = lib/calcos/calcos
 
 [extension=calcos.ccos]
 sources = src/ccos.c
-define_macros = 
+define_macros =
 	NUMPY = 1
 include_dirs = numpy
 
@@ -35,7 +36,7 @@ include_dirs = numpy
 pre-hook.numpy-extension-hook = stsci.distutils.hooks.numpy_extension_hook
 
 [global]
-setup_hooks = 
+setup_hooks =
 	stsci.distutils.hooks.use_packages_root
 	stsci.distutils.hooks.tag_svn_revision
 	stsci.distutils.hooks.version_setup_hook

defsetup.py
@@ -11,21 +11,20 @@ except ImportError:
 pkg = "calcos"
 
 setupargs = {
-    'version' :			"1.0",
+    'version' :			"3.1.2",
     'description' :		"C extension module for calcos",
-    'author' :			"Phil Hodge",
+    'author' :			"Phil Hodge and Robert Jedrzejewski",
     'author_email' :	"help@stsci.edu",
     'platforms' :		["Linux", "Solaris", "Mac OS X", "Windows"],
     'scripts' :			['lib/calcos/calcos'],
     'package_dir' :     { 'calcos' : 'lib/calcos', },
 
-    'ext_modules' :		[ 
+    'ext_modules' :		[
                             distutils.core.Extension ("calcos.ccos", ["src/ccos.c"],
                                define_macros = [('NUMPY', '1')],
-                               include_dirs = [ distutils.sysconfig.get_python_inc(), 
+                               include_dirs = [ distutils.sysconfig.get_python_inc(),
                                                 numpy.get_include()
                                               ]
                             )
                         ],
 }
-

commit 0fc4850519e906ed8734630c0ed38ab5f50fee16

Fix wrong usage of boxcar introduced by r49353
git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/calcos/trunk@49586 fe389314-cf27-0410-b35b-8c050e845b92

lib/calcos/wavecal.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, division         # confidence high
 import math
 import os
 import numpy as np
-from scipy.signal import boxcar
+from scipy import signal as scipysignal
+from scipy import ndimage
 import astropy.io.fits as fits
 from .calcosparam import *
 from . import ccos
@@ -969,7 +970,8 @@ def ttFindSpec(xdisp, xtract_info, life_adj_offset, xd_range, box):
         y0 = max(y0, 0)
         y1 = min(y1, len(xdisp) - 1)
 
-    xdisp_sm = boxcar(xdisp, (box,), mode="nearest")
+    boxcar_kernel = scipysignal.boxcar(box) / box
+    xdisp_sm = ndimage.convolve(xdisp, boxcar_kernel, mode="nearest")
     len_xdisp_sm = len(xdisp_sm)
 
     if y0 >= y1:

lib/calcos/xd_search.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division         # confidence unknown
 import numpy as np
-from scipy.signal import boxcar
+from scipy import signal
+from scipy import ndimage
 from . import cosutil
 from .calcosparam import *       # parameter definitions
 from . import ccos
@@ -246,7 +247,8 @@ def findPeak(e_j, box):
         profile
     """
 
-    e_j_sm = boxcar(e_j, (box,), mode="nearest")
+    boxcar_kernel = signal.boxcar(box) / box
+    e_j_sm = ndimage.convolve(e_j, boxcar_kernel, mode="nearest")
 
     index = np.argsort(e_j_sm)
     ymax = index[-1]

setup.cfg
@@ -1,7 +1,7 @@
 [metadata]
 name = calcos
 version = 3.1.2
-vdate = 2016-02-24
+vdate = 2016-03-02
 author = Phil Hodge and Robert Jedrzejewski
 author-email = help@stsci.edu
 summary = Calibration software for COS (Cosmic Origins Spectrograph)