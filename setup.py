90ef3f3fe843303cc42e4c30a2ae372eedf4e6e6
Joseph Hunkeler <jhunkeler@gmail.com>
2019-08-22 09:19:27 -0400

    Refactor package structure

--- setup.py
+++ setup.py
@@ -1,17 +1,81 @@
 #!/usr/bin/env python
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distribute_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup
+import os
+from fnmatch import fnmatch
+from setuptools import setup, find_packages, Extension
+from numpy import get_include as numpy_includes
+
+
+def c_sources(parent):
+    sources = []
+    for root, _, files in os.walk(parent):
+        for f in files:
+            fn = os.path.join(root, f)
+            if fnmatch(fn, '*.c'):
+                sources.append(fn)
+    return sources
+
+
+def c_includes(parent, depth=1):
+    includes = [parent]
+    for root, dirs, _ in os.walk(parent):
+        for d in dirs:
+            dn = os.path.join(root, d)
+            if len(dn.split(os.sep)) - 1 > depth:
+                continue
+            includes.append(dn)
+    return includes
+
+
+PACKAGENAME = 'calcos'
+SOURCES = c_sources('src')
+INCLUDES = c_includes('src') + [numpy_includes()]
 
 
 setup(
-    setup_requires=['d2to1>=0.2.5', 'stsci.distutils>=0.3.2'],
-    tests_require=['pytest'],
-    d2to1=True,
-    use_2to3=False,
-    zip_safe=False
+    name=PACKAGENAME,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'astropy>=1.1.1',
+        'numpy>=1.10.1',
+        'scipy>=0.14',
+    ],
+    extras_require={
+        'docs': [
+            'sphinx',
+        ],
+        'test': [
+            'pytest',
+            'pytest-cov',
+            'codecov',
+        ],
+    },
+    packages=find_packages(),
+    package_data={
+        PACKAGENAME: [
+            'pars/*',
+            '*.help',
+        ],
+    },
+    ext_modules=[
+        Extension(
+            PACKAGENAME + '._ccos',
+            sources=SOURCES,
+            include_dirs=INCLUDES,
+        ),
+    ],
+    author='Phil Hodge, Robert Jedrzejewski',
+    author_email='help@stsci.edu',
+    description='Calibration software for COS (Cosmic Origins Spectrograph)',
+    url='https://github.com/spacetelescope/calcos',
+    license='BSD',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: C',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
