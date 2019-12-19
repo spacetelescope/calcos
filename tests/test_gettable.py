d0c29515842aa71fcf43881e3a2073cc83397e6c
James White <jwhite@stsci.edu>
2019-12-19 10:31:35 -0500

    Add fixture to test multiple cenwaves for cenwave wildcard row

--- tests/test_gettable.py
+++ tests/test_gettable.py
@@ -35,6 +35,11 @@ def variable_cenwave_tds_filter(tdstab_table, request):
     return {'opt_elem': 'G130M', 'aperture': 'PSA', "cenwave": cenwave, "segment": 'FUVA'}, tdstab_table[idx]
 
 
+@pytest.fixture(params=[10, 20])
+def wildcard_cenwave_tds_filter(tdstab_table, request):
+    return {'opt_elem': 'G185M', 'aperture': 'PSA', 'cenwave': request.param, 'segment': 'NUVA'}
+
+
 def test_cenwave_selection_tds(variable_cenwave_tds_filter, tdstab_file):
     """Test TDSTAB row selection for variable cenwave value. getTable arguments match usage from  doFluxCorr where the
     TDSTAB is used.
@@ -45,9 +50,8 @@ def test_cenwave_selection_tds(variable_cenwave_tds_filter, tdstab_file):
     compare_rec_to_record(result, expected_result)
 
 
-def test_cenwave_wildcard_selection(tdstab_table, tdstab_file):
-    test_filter = {'opt_elem': 'G185M', 'aperture': 'PSA', 'cenwave': 10, 'segment': 'NUVA'}
-    expected = tdstab_table[-1]
-    result = getTable(tdstab_file, test_filter, exactly_one=True)
+def test_cenwave_wildcard_selection(wildcard_cenwave_tds_filter, tdstab_file, tdstab_table):
+    expected = tdstab_table[-1]  # Should get the same row for variable cenwaves for G185M/PSA/NUVA
+    result = getTable(tdstab_file, wildcard_cenwave_tds_filter, exactly_one=True)
 
     compare_rec_to_record(result, expected)
