eb5809ce9944ae10ba88023b774f8bd31ee53cb1
Rachel Plesha <rplesha@stsci.edu>
2021-03-05 11:02:59 -0500

    That was not intended for this instance. We should add a protection to pushing to the master branch.

--- setup.py
+++ setup.py
@@ -5,8 +5,6 @@ from fnmatch import fnmatch
 from setuptools import setup, find_packages, Extension
 from numpy import get_include as numpy_includes
 
-print('test')
-
 def c_sources(parent):
     sources = []
     for root, _, files in os.walk(parent):
