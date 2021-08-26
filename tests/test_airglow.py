5e68e84ab0021acc04ab2eb66b2f46d60564ceaa
asfaw-michael <masfaw@stsci.edu>
2021-08-25 21:13:50 -0400

    created test files for airglow.py, average.py, phot.py, and shiftfile.py

---
+++ tests/test_airglow.py
@@ -0,0 +1,7 @@
+def test_find_airglow_limits():
+    # Setup
+    inf = ''
+    seg = ["FUVA", "FUVB", "NUVA", "NUVB", "NUVC"]
+    # todo declare disptab and airglow_line
+    # Test
+    # Verify
---
+++ tests/test_phot.py
@@ -0,0 +1,6 @@
+def test_do_phot():
+    assert False
+
+
+def test_read_im_pht_tab():
+    assert False
