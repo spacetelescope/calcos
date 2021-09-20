1fbaf352c19724be312ca2dd6491b64593a9763a
Michael Asfaw <masfaw@stsci.edu>
2021-09-19 22:41:50 -0400

    finished test_airglow.py

--- tests/test_shiftfile.py
+++ tests/test_shiftfile.py
@@ -15,14 +15,14 @@ def test_get_shifts():
     # Setup
     shift_file = "shift_file.txt"
     ob = shiftfile.ShiftFile(shift_file, 'lco721egq', 'any')
-    keys = [(2, 'any'), (1, 'nuva'), (2, 'nuvc'), (1, 'any')]
-    expected_values = [((-41.67250061, 10.0), 3), ((-42.35122681, 10.0), 1),
+    keys = [(2, 'nuva'), ('any', 'nuvb'), (2, 'nuvc'), (1, 'any')]
+    expected_values = [((-41.85122681, 10.0), 1), ((-40.95245361, 10.0), 2),
                        ((-41.67250061, 10.0), 1), ((-41.17250061, 10.0), 3)]
-    # todo change the values
     # Test
     test_values = []
     for key in keys:
         test_values.append(shiftfile.ShiftFile.getShifts(ob, key))
     # Verify
+    print(test_values)
     for i in range(len(expected_values)):
         assert expected_values[i] == test_values[i]
