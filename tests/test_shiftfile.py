0b01e02524d2926752d93fa16b312abddfc19910
Michael Asfaw <masfaw@stsci.edu>
2021-11-15 11:03:29 -0500

    added fuv tests in test_shiftfile.py

--- tests/test_shiftfile.py
+++ tests/test_shiftfile.py
@@ -12,6 +12,10 @@ def test_shift_file():  # Tests the initialized variables
                 file.write("{}\t{}\t{}\t{}\t{}\t{}\n".format("abc123def", "any", "1", "NUVA", "45.234435", "7"))
             elif i % 5 == 0:
                 file.write("{}\t{}\t{}\t{}\t{}\t{}\n".format("ghi456jkl", "any", "2", "NUVB", "34.543453", "7"))
+            elif i % 6 == 0 or i % 8 == 0:
+                file.write("{}\t{}\t{}\t{}\t{}\t{}\n".format("ghi456jkl", "any", "2", "FUVA", "19.543453", "5"))
+            elif i == 9:
+                file.write("{}\t{}\t{}\t{}\t{}\t{}\n".format("mno789pqr", "any", "2", "FUVB", "52.723453", "6"))
             else:
                 file.write("{}\t{}\t{}\t{}\t{}\t{}\n".format("mno789pqr", "any", "1", "NUVC", "-34.543453", "7"))
 
@@ -26,11 +30,9 @@ def test_get_shifts():
     shift_file = "shift_file.txt"
     ob1 = shiftfile.ShiftFile(shift_file, 'ghi456jkl', 'any')
     ob2 = shiftfile.ShiftFile(shift_file, 'abc123def', 'any')
-    keys = [('any', 'nuva'), ('any', 'nuvb'), (2, 'nuvc'), ('any', 'any')]
-    expected_values1 = [((None, None), 0), ((34.543453, 7.0), 1),
-                       ((None, None), 0), ((34.543453, 7.0), 1)]
-    expected_values2 = [((45.234435, 7.0), 1), ((None, None), 0),
-                        ((None, None), 0), ((45.234435, 7.0), 1)]
+    keys = [('any', 'nuva'), ('any', 'nuvb'), (2, 'nuvc'), ('any', 'any'), ('any', 'fuva'), ('any', 'fuvb')]
+    expected_values1 = [((None, None), 0), ((34.543453, 7.0), 1), ((None, None), 0), ((34.543453, 7.0), 1), ((None, None), 0), ((None, None), 0)]
+    expected_values2 = [((45.234435, 7.0), 1), ((None, None), 0), ((None, None), 0), ((45.234435, 7.0), 1), ((None, None), 0), ((None, None), 0)]
     # Test
     test_values1 = []
     test_values2 = []
