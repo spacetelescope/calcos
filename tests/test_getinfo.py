1972722048e6eb2995740cd85c089af9955479bc
Michael Asfaw <masfaw@stsci.edu>
2022-01-17 21:56:06 -0500

    created a test script for getinfo.py

---
+++ tests/test_getinfo.py
@@ -0,0 +1,18 @@
+def test_initial_info():
+    assert False
+
+
+def test_get_general_info():
+    assert False
+
+
+def test_get_switch_values():
+    assert False
+
+
+def test_get_ref_file_names():
+    assert False
+
+
+def test_reset_switches():
+    assert False
