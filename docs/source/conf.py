6f630c3c68927b0b7bb25a95a679cc53df306b42
Zach Burnett <zburnett@stsci.edu>
2023-01-19 13:51:58 -0500

    move build configuration into `pyproject.toml`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,12 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-
+from datetime import datetime
 import os
 import sys
+from pathlib import Path
+
+import tomli
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
